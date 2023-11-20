b8b8d38038feaddb0db19a5d5b1004f861ce10af
sacredtreasure <selfharm777@protonmail.com>
2023-11-20 02:27:00 +0100

    working code: tested

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::env;
+// use std::env;
 use tokio;
 
-use serenity::{ async_trait, model::{channel::Message, gateway::Ready}, prelude::*,};
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
 
 const HELP_MESSAGE: &str = "
     Hello there, fellow degen!
@@ -9,7 +13,7 @@ const HELP_MESSAGE: &str = "
     I'm here to help you to get up to speed in this server and also help you get what you need.
 
     ? Need Alpha and insights? 
-    - Post in the <#CHANNEL_ID> channel and our top degen specialists will assist you.
+    - Post in the <#1176593229165105162> channel and our top degen specialists will assist you.
 
     ? Need the next 100x? 
     - This should help - <https://dexscreener.com>
@@ -23,31 +27,33 @@ const HELP_MESSAGE: &str = "
 
 ";
 
-const HELP_COMMAND: &str = "!degen";
+
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content = HELP_COMMAND {
+        if msg.content == "!degen" {
             if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
-                println!("Error sending message: {:?}" why);
+                println!("Error sending message: {:?}", why);
             }
         }
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!" ready.user.name);
+        println!("{} is connected!", ready.user.name);
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let token = env::var("DISCORD_TOKEN")
-    .expect("Expected a token in the environment...");
+    // let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment...");
+     let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::new(&token)
+    let mut client = Client::builder("DISCORD_TOKEN", intents)
         .event_handler(Handler)
         .await
         .expect("Error creating client...");
@@ -56,5 +62,3 @@ async fn main() {
         println!("Client error: {:?}", why);
     }
 }
-
-
